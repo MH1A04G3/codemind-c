cd5058cd6869b0ef2875787979fa88facacceefc
MH1A04G3 <120240943+MH1A04G3@users.noreply.github.com>
2023-01-14 11:16:58 +0530

    #include<stdio.h> int main() {     float b,pf,da,hra,gs;     scanf("%f%f%f",&b,&da,&hra);     pf=(b*12)/100;     gs=pf+da+hra+b;     printf("%0.2f %0.2f ",pf,gs); }

--- Calculate_Gross_Salary_of_an_employee.c
+++ Calculate_Gross_Salary_of_an_employee.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-float b,pf,da,hra,gs;
-scanf("%f%f%f",&b,&da,&hra);
-pf=(b*12)/100;
-gs=pf+da+hra+b;
-printf("%0.2f
+    float b,pf,da,hra,gs;
+    scanf("%f%f%f",&b,&da,&hra);
+    pf=(b*12)/100;
+    gs=pf+da+hra+b;
+    printf("%0.2f
 %0.2f
 ",pf,gs);
 }
