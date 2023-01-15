1eecbf1aaabff698fd3e67c407d5761895e835e6
MH1A04G3 <120240943+MH1A04G3@users.noreply.github.com>
2023-01-15 20:17:51 +0530

    #include<stdio.h> int main() {     int n,a=2,b=6;     scanf("%d",&n);     if(n%2!=0)     {         printf("weird");     }     else if(n%2==0&&a<=5)     {         printf("not weird");     }     else if(n%2==0&&a<=20)     {         printf("weird");     }     else if(n%2==0&&a>=20); }

---
+++ Weird_or_not_Weird.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,a=2,b=6;
+    scanf("%d",&n);
+    if(n%2!=0)
+    {
+        printf("weird");
+    }
+    else if(n%2==0&&a<=5)
+    {
+        printf("not weird");
+    }
+    else if(n%2==0&&a<=20)
+    {
+        printf("weird");
+    }
+    else if(n%2==0&&a>=20);
+}
