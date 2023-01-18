f9ff693a7cda93526c72296d986602067634cbf8
MH1A04G3 <120240943+MH1A04G3@users.noreply.github.com>
2023-01-18 20:16:45 +0530

    #include<stdio.h> int main() {     int a,b,x,y;     scanf("%d",&a);     if(x>=a%2*b+y)     {         printf("Qualify");     }     else     {         printf("Not Qualify");     } }

---
+++ Contest:.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,x,y;
+    scanf("%d",&a);
+    if(x>=a%2*b+y)
+    {
+        printf("Qualify");
+    }
+    else
+    {
+        printf("Not Qualify");
+    }
+}
