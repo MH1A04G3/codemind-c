6484647ce9cd68ffb33c8a71a522127edc365c28
MH1A04G3 <120240943+MH1A04G3@users.noreply.github.com>
2023-01-27 11:49:19 +0530

    #include<stdio.h> int main() {     int a,b,c;     scanf("%d%d",&a,&b);     c=21-(a+b);     if(c>10)     {         printf("%d",-1);     }     else     {         printf("%d",c);     } }

--- Blackjack.c
+++ Blackjack.c
@@ -3,13 +3,13 @@ int main()
 {
     int a,b,c;
     scanf("%d%d",&a,&b);
-     c=21-(a+b);
-     if(c>10)
-   {
-    printf("%d",-1);
-}
-else
-{
-    printf("%d",c);
-}
+    c=21-(a+b);
+    if(c>10)
+    {
+        printf("%d",-1);
+    }
+    else
+    {
+        printf("%d",c);
+    }
 }
