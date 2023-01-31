735e29e721c82b7be88d64794a172f24a1d2847e
MH1A04G3 <120240943+MH1A04G3@users.noreply.github.com>
2023-01-31 21:00:55 +0530

    #include<stdio.h> int main() {     int n,rem,square,sum=0;     scanf("%d",&n);     square=(n*n);     while(square!=0)     {         rem=square%10;         sum=sum+rem;         square=square/10;     }     if(sum==n)     {         printf("Neon Number");     }     else     {         printf("Not Neon Number");     } }

--- Neon_Number.c
+++ Neon_Number.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int n,rem,sum=0,square;
+    int n,rem,square,sum=0;
     scanf("%d",&n);
     square=(n*n);
     while(square!=0)
     {
         rem=square%10;
         sum=sum+rem;
-        square=square/10;;
+        square=square/10;
     }
     if(sum==n)
     {
